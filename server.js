d278f1e4d4ef781ad34e730a6799279b87f8e42f
Johnny Messias Paulino <90730926+JohnnyMessiasPaulino@users.noreply.github.com>
2023-05-07 18:03:38 -0300

    Teste github.io

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/telaLogin', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
